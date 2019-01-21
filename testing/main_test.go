af207eda8f3df19e56cf59814b8c581e65a611d9
Aniket Singh <aniketsingh0104@gmail.com>
2019-01-21 16:03:51 +0530

    Test cases for email

--- testing/main_test.go
+++ testing/main_test.go
@@ -1,70 +1,84 @@
 package main
 
 import (
+	"context"
+	pb "notification/proto"
 	"testing"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/any"
+	"github.com/golang/protobuf/ptypes/empty"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestSendEmailNotifications(t *testing.T) {
+func testSendEmailNotifications(t *testing.T, client pb.NotificationsSenderClient) {
+	ctx := context.Background()
 
 	Convey("receives an event (Testing SendNotifications)", t, func() {
 
-		Convey("receiving event with invalid notification id", nil)
-
-	})
-
-	Convey("receives an event", t, func() {
+		Convey("invalid notification id should return error", func() {
+			test := &empty.Empty{}
+			serialized, err := proto.Marshal(test)
+			if err != nil {
+				t.Fatal("could not serialize empty")
+			}
+			newNotification := &pb.NotificationRequest{
+				NotificationId: "",
+				NotificationData: &any.Any{
+					TypeUrl: proto.MessageName(test),
+					Value:   serialized,
+				},
+			}
+			resp, _ := client.SendNotifications(ctx, newNotification)
+			So(resp.ErrorStatus, ShouldEqual, "Invalid NotificationId")
+		})
 
 		Convey("event is valid, forwarding event (Testing SendNotificationEmails)", func() {
 
-			Convey("receiving event with invalid type", nil)
+			newNotification := &pb.NotificationRequest{}
 
-		})
+			Convey("receiving event with invalid type should return error", func() {
 
-	})
+			})
 
-	Convey("receives an event", t, func() {
+			Convey("receiving email event with no recipients email ids should return error", nil)
 
-		Convey("event is valid, forwarding event (Testing SendNotificationEmails)", func() {
+			Convey("getting invalid email template should return error", func() {
+				test := &empty.Empty{}
+				serialized, err := proto.Marshal(test)
+				if err != nil {
+					t.Fatal("could not serialize empty")
+				}
+				newNotification.NotificationId = /*some random id*/ ""
+				newNotification.NotificationData = &any.Any{
+					TypeUrl: proto.MessageName(test),
+					Value:   serialized,
+				}
+				_, err = client.SendNotifications(ctx, newNotification)
+				So(err, ShouldNotBeNil)
+			})
 
-			Convey("receiving email event with no recipients email ids", nil)
+			Convey("email template rendering error should return error", nil)
+			Convey("getting valid email template and rendering successful, forwarding email", func() {
 
-		})
-
-	})
-
-	Convey("receives an event", t, func() {
-
-		Convey("event is valid, forwarding event (Testing SendNotificationEmails)", func() {
-
-			Convey("getting invalid email template", nil)
-
-		})
-
-	})
-
-	Convey("receives an event", t, func() {
-
-		Convey("event is valid, forwarding event (Testing SendNotificationEmails)", func() {
-
-			Convey("email template rendering error", nil)
-
-		})
-
-	})
-
-	Convey("receives an event", t, func() {
-
-		Convey("event is valid, forwarding event", func() {
-
-			Convey("getting valid email template, forwarding email", func() {
-
-				Convey("email template rendering successful", func() {
-
-					Convey("Send email with proper recipients email ids", nil)
-
+				Convey("Sending email with invalid structure should return error", func() {
+					test := &empty.Empty{}
+					serialized, err := proto.Marshal(test)
+					if err != nil {
+						t.Fatal("could not serialize empty")
+					}
+					newNotification.NotificationId = /*some random id*/ ""
+					newNotification.NotificationData = &any.Any{
+						TypeUrl: proto.MessageName(test),
+						Value:   serialized,
+					}
+					_, err = client.SendNotifications(ctx, newNotification)
+					So(err, ShouldNotBeNil)
 				})
+
+				Convey("Sending email with no proper recipients", nil)
+
+				Convey("Send email with proper recipients email ids", nil)
 
 			})
 
